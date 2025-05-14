797ff831a68f9893ca24d363cd6ae9c2be908a9e
nguyen minh dang <nguyenminhdangdev@gmail.com>
2025-05-14 18:16:55 +0700

    toi uu avatar

--- server.py
+++ server.py
@@ -82,7 +82,7 @@ class APIHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
 if __name__ == '__main__':
-    server_address = ('', 8080)
+    server_address = ('', 8081)
     httpd = HTTPServer(server_address, APIHandler)
-    print('Server running on port 8080...')
+    print('Server running on port 8081...')
     httpd.serve_forever() 
